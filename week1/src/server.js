6f2e38941472a87fe0c371d7f0edf489af46175b
Dawn Monroe <deetriplett@gmail.com>
2017-01-26 06:47:32 -0500

    added config require() and app.use name function

--- week1/src/server.js
+++ week1/src/server.js
@@ -1,12 +1,17 @@
 // src/server.js
 const express = require('express');
+const config =require('./config');
 
 const app = express();
 
+app.use('./hello/:name', function(req, res, next) {
+  res.end('Hello ${req.params.name}');
+}
+        
 app.use(function(req, res, next) {
   res.end("Documentation http://expressjs.com/");
 });
 
 app.listen(8080, function() {
-  console.log("I AM LISTENING!!!");
+  console.log("I AM LISTENING ON PORT" + port.name + " !!!");
 });
