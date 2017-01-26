434bb49ecd74578cf9b70b7dd3808d35aab757cd
Dawn Monroe <deetriplett@gmail.com>
2017-01-26 06:50:31 -0500

    added app response html

--- week1/src/server.js
+++ week1/src/server.js
@@ -8,10 +8,22 @@ app.use('./hello/:name', function(req, res, next) {
   res.end('Hello ${req.params.name}');
 }
         
-app.use(function(req, res, next) {
-  res.end("Documentation http://expressjs.com/");
+app.use(function (req, res, next) {
+  res.format({
+    html: ()=> res.send(`
+    <h1>Our Project</h1>
+    <ul>
+    <li>Get list of files (inc meta data)</li>
+    <li>Upload file</li>
+    <li>Edit file</li>
+    <li>Delete FIle</li>
+    </ul>
+    `)
+  })
+  res.end('Documentation http://express.js.com/');
 });
 
+
 app.listen(8080, function() {
-  console.log("I AM LISTENING ON PORT" + port.name + " !!!");
+  console.log("I AM LISTENING ON PORT" + config.port + " !!!");
 });
